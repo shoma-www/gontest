4da3ea47dac3187b954e857dc7fa4c284371ba9b
shoma-www <shumai.1215.2018@gmail.com>
2020-02-26 04:56:00 +0900

    Add Dijkstra Use Priority Queue

--- algorithm/shortestpath/dijkstra.go
+++ algorithm/shortestpath/dijkstra.go
@@ -2,6 +2,9 @@ package shortestpath
 
 import (
 	"math"
+	"strconv"
+
+	"github.com/shoma-www/my-contest-go/algorithm/structure"
 )
 
 // Dijkstra ダイクストラ法
@@ -11,7 +14,7 @@ import (
 // 通常の実装：O(|V|^2)  ※非常に密なグラフに対して優位
 // 優先度付きキュー：O(|E|log|V|)
 // 参考 http://www.thothchildren.com/chapter/5b6ae3282787593b86358a54
-func Dijkstra(edges Edges, vertex, start, end int) ([]int, error) {
+func Dijkstra(edges Edges, vertex, start, end int) []int {
 	// 求めたい経路のコストを初期化
 	d := make([]int, vertex)
 	for i := 0; i < vertex; i++ {
@@ -28,7 +31,7 @@ func Dijkstra(edges Edges, vertex, start, end int) ([]int, error) {
 			cost: e.cost,
 		}
 		graph[toE.from] = append(graph[toE.from], toE)
-	}	
+	}
 
 	used := make([]bool, vertex)
 	for i := 0; i < vertex; i++ {
@@ -44,7 +47,7 @@ func Dijkstra(edges Edges, vertex, start, end int) ([]int, error) {
 			d[e.to] = int32min(d[e.to], d[id] + e.cost)
 		}
 	}
-	
+
 	// 最短経路の出力
 	p := make([]int, 1)
 	p[0] = end
@@ -62,7 +65,76 @@ func Dijkstra(edges Edges, vertex, start, end int) ([]int, error) {
 		}
 	}
 
-	return p, nil
+	return p
+}
+
+// DijkstraP ダイクストラ法(優先度付きキュー)
+// 負のコストを含まないグラフで利用
+// コストが低いノードのみを処理することでベルマンフォード法より高速
+// 計算量
+// 通常の実装：O(|V|^2)  ※非常に密なグラフに対して優位
+// 優先度付きキュー：O(|E|log|V|)
+// 参考 http://www.thothchildren.com/chapter/5b6ae3282787593b86358a54
+func DijkstraP(edges Edges, vertex, start, end int) []int {
+	// 求めたい経路のコストを初期化
+	d := make([]int, vertex)
+	for i := 0; i < vertex; i++ {
+		d[i] = math.MaxInt32
+	}
+	d[start] = 0
+
+	structure.NewPriorityQueue()
+
+	graph := make([]Edges, vertex)
+	for _, e := range edges {
+		graph[e.from] = append(graph[e.from], e)
+		toE := Edge{
+			from: e.to,
+			to: e.from,
+			cost: e.cost,
+		}
+		graph[toE.from] = append(graph[toE.from], toE)
+	}
+
+	q := structure.NewPriorityQueue()
+	q.Push(&structure.Item{
+		Priority: 0,
+		Value: strconv.Itoa(start),
+		Index: 0,
+	})
+	for !q.Empty() {
+		item := q.Pop()
+		value, _ := strconv.Atoi(item.Value)
+		if d[value] < item.Priority { continue }
+		for _, e := range graph[value] {
+			if d[e.to] > d[value] + e.cost {
+				d[e.to] = d[value] + e.cost
+				q.Push(&structure.Item{
+					Priority: d[e.to],
+					Value: strconv.Itoa(e.to),
+				})
+			}
+		}
+	}
+
+	// 最短経路の出力
+	p := make([]int, 1)
+	p[0] = end
+	pos := end
+	cost := d[end]
+	for cost > 0 {
+		for _, e := range graph[pos] {
+			if d[e.to] == cost - e.cost {
+				p = append(p[0:1], p[0:]...)
+				p[0] = e.to
+				pos = e.to
+				cost = d[e.to]
+				break
+			}
+		}
+	}
+
+	return p
 }
 
 func int32min(x, y int) int {
--- algorithm/shortestpath/dijkstra_test.go
+++ algorithm/shortestpath/dijkstra_test.go
@@ -1,7 +1,6 @@
 package shortestpath
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 	"testing"
@@ -49,12 +48,63 @@ func TestDijkstra(t *testing.T) {
 			}
 			edges = append(edges, edge)
 		}
-		ans, err := Dijkstra(edges, c.vertex, c.start, c.end)
-		fmt.Println(ans)
+		ans := Dijkstra(edges, c.vertex, c.start, c.end)
 
-		if err != nil {
-			t.Errorf("dame %v.", err)
+		if len(c.exp) != len(ans) {
+			t.Errorf("dame len exp:%v ans:%v .", c.exp ,ans)
 		}
+		for i := range ans {
+			if c.exp[i] != ans[i] {
+				t.Errorf("dame %v %v.", c.exp[i], ans[i])
+			}
+		}
+	}
+}
+
+func TestDijkstraP(t *testing.T) {
+	testCase := []struct{
+		edges []string
+		vertex int
+		start int
+		end int
+		exp []int
+	}{
+		{
+			edges: []string{
+				"0 1 2",
+				"0 2 4",
+				"1 4 7",
+				"1 3 3",
+				"2 3 2",
+				"2 5 2",
+				"3 4 3",
+				"4 5 1",
+				"4 6 2",
+				"5 6 3",
+			},
+			vertex: 7,
+			start: 0,
+			end: 6,
+			exp: []int{ 0, 2, 5, 4, 6 },
+		},
+	}
+
+	for _, c := range testCase {
+		edges := make(Edges, 0, len(c.edges))
+		for _, e := range c.edges {
+			a := strings.Split(e, " ")
+			from, _ := strconv.Atoi(a[0])
+			to, _ := strconv.Atoi(a[1])
+			cost, _ := strconv.Atoi(a[2])
+			edge := Edge{
+				from: from,
+				to: to,
+				cost: cost,
+			}
+			edges = append(edges, edge)
+		}
+		ans := DijkstraP(edges, c.vertex, c.start, c.end)
+
 		if len(c.exp) != len(ans) {
 			t.Errorf("dame len exp:%v ans:%v .", c.exp ,ans)
 		}
