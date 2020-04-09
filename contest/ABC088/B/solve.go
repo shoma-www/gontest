f0cb239716444cd167d19c52c323f83c3dae557e
shoma-www <shumai.1215.2018@gmail.com>
2020-04-10 01:52:30 +0900

    ABC088 A/B Resolve

---
+++ contest/ABC088/B/solve.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	solve(os.Stdin, os.Stdout)
+}
+
+func solve(in io.Reader, out io.Writer) {
+	bs := NewBufScanner(in)
+	bw := NewBufWriter(out)
+	defer bw.w.Flush()
+	n := bs.IntScan()
+	cards := []int{}
+	for i:=0; i<n; i++ {
+		cards = append(cards, bs.IntScan())
+	}
+	sort.Ints(cards)
+
+	alice := 0
+	for i:=n-1; i>=0; i-=2 {
+		alice += cards[i]
+	}
+
+	bob := 0
+	for i:=n-2; i>=0; i-=2 {
+		bob += cards[i]
+	}
+
+	bw.Printf("%v", alice - bob)
+}
+
+// BufScanner original scanner
+type BufScanner struct {
+	s *bufio.Scanner
+}
+
+// NewBufScanner constructer
+func NewBufScanner(in io.Reader) *BufScanner {
+	s := bufio.NewScanner(in)
+	s.Buffer(make([]byte, 1024), 1e+9)
+	s.Split(bufio.ScanWords)
+	return &BufScanner{
+		s: s,
+	}
+}
+
+// Scan Scan Data
+func (b *BufScanner) Scan() string {
+	b.s.Scan()
+	return b.s.Text()
+}
+
+// IntScan Scan Data
+func (b *BufScanner) IntScan() int {
+	v, err := strconv.Atoi(b.Scan())
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+// BufWriter original writer
+type BufWriter struct {
+	w *bufio.Writer
+}
+
+// NewBufWriter constructer
+func NewBufWriter(out io.Writer) *BufWriter {
+	w := bufio.NewWriter(out)
+	return &BufWriter{
+		w: w,
+	}
+}
+
+// Printf Output file
+func (b *BufWriter) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(b.w, format, a...)
+}
---
+++ contest/ABC088/D/solve_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/shoma-www/my-contest-go/common/test"
+)
+
+func TestSolve(t *testing.T) {
+	fmt.Println("########## Start Test. ##########")
+	dir, _ := filepath.Abs("./testdata")
+	tb := test.NewTestBook(dir)
+
+	all, success := 0, 0
+	for _, testName := range tb.GetTestList() {
+		fmt.Printf("%s:\n", testName)
+		all++
+		if t.Run(testName, func(t *testing.T){
+			// 通常
+			// test.Execute(t, solve, testName, tb[testName], 2000)
+
+			// 対話式
+			// test.InteractiveTestExecute(t, solve, testName, tb[testName], ans, 2000, '!')
+		}) {
+			success++
+		}
+	}
+
+	fmt.Printf("All: %d Success: %d Error: %d\n", all, success, all-success)
+	fmt.Println("########## Finish Test. ##########")
+}
+
+func ans(buf *test.SyncBuffer, v *test.Path, q string) {
+
+}
